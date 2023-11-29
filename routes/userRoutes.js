df20dd8af459ea78af7a8a0d6737082c6223d7f6
amardip padghan <82278378+amardippadghan@users.noreply.github.com>
2023-11-29 11:33:36 +0530

    updating userRoute

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user"); // Import the user schema
 const jwt = require("jsonwebtoken");
+const StudentInfo = require("../models/student");
+
 
 
 const router = express.Router();
@@ -66,5 +68,94 @@ router.delete("/:id",  async (req, res) => {
     res.status(500).json({ error: "Something went wrong" });
   }
 });
+
+router.get("/studentinfo" , async(req,res)=>{
+   try {
+    const allStudents = await StudentInfo.find();
+    res.json(allStudents);
+  } catch (error) {
+    res.status(500).json({ error: "Could not retrieve students" });
+  }
+
+})
+
+router.post("/studentinfo" , async(req,res)=>{
+  try {
+    const newStudent = await StudentInfo.create(req.body);
+    res.json(newStudent);
+  } catch (error) {
+    res.status(500).json({ error: "Could not create student" });
+  }
+
+}
+)
+router.get("/studentinfo/findByUserID/:userID", async (req, res) => {
+  const { userID } = req.params;
+
+  try {
+    const students = await StudentInfo.find({ userID });
+
+    if (!students || students.length === 0) {
+      return res
+        .status(404)
+        .json({ error: "No students found for this user ID" });
+    }
+
+    res.json(students);
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+router.put("/studentinfo/:id", async (req, res) => {
+  const studentId = req.params.id;
+  const updatedFields = req.body;
+
+  try {
+    const existingStudent = await StudentInfo.findById(studentId);
+
+    if (!existingStudent) {
+      return res.status(404).json({ error: "Student not found" });
+    }
+
+    // Update PersonalInfo fields if present in the request body
+    if (updatedFields.PersonalInfo) {
+      Object.assign(existingStudent.PersonalInfo, updatedFields.PersonalInfo);
+    }
+
+    // Update EducationalDetails fields if present in the request body
+    if (updatedFields.EducationalDetails) {
+      existingStudent.EducationalDetails = updatedFields.EducationalDetails;
+    }
+
+    // Update TechnicalSkills fields if present in the request body
+    if (updatedFields.TechnicalSkills) {
+      Object.assign(
+        existingStudent.TechnicalSkills,
+        updatedFields.TechnicalSkills
+      );
+
+      
+    }
+
+    // Save the updated student document
+    const updatedStudent = await existingStudent.save();
+
+    res.json({
+      message: "Student updated successfully",
+      student: updatedStudent,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
+
+
+
+
+
+
+
 
 module.exports = router;
